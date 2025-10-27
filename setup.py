e8c785aa0a7cf62a3011d40eaec5cdb6bac60268
Julio Cesar <juliocesargcosta123@gmail.com>
2025-10-27 19:01:40 +0000

    fix/initial_release
    - First fixes on README file.
    - Added Ontological Framework diagram.
    - Added ontology concrete sample.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name='ontologytoapi',
-    version='0.0.3',
+    version='0.0.4',
     description='Multi purpose API Generator based on an Ontology Framework.',
     long_description=
     f"{open('README.md').read()}\n\n" +
